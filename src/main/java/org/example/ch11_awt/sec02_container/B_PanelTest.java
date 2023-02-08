a0daec34f524d020bdd82d963dd6c7bb6d37543d
ljy <liangjy881@naver.com>
2023-02-09 00:24:31 +0900

    开始AWT 容器

---
+++ src/main/java/org/example/ch11_awt/sec02_container/B_PanelTest.java
@@ -0,0 +1,20 @@
+package org.example.ch11_awt.sec02_container;
+
+import java.awt.*;
+
+public class B_PanelTest {
+    public static void main(String[] args) {
+        var f = new Frame("测试窗口");
+        // 创建一个Panel容器
+        var p = new Panel();
+        // 向Panel容器中添加两个组建
+        p.add(new TextField(20));
+        p.add(new Button("单击我"));
+        // 将Panel容器添加到Frame窗口中
+        f.add(p);
+        // 设置窗口的大小，位置
+        f.setBounds(30, 30, 250, 120);
+        // 将窗口显示出来(Frame对象默认处于隐藏状态)
+        f.setVisible(true);
+    }
+}
